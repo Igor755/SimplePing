c315120ab3b80e8737a29162bd851e2742fcaa6f
DESKTOP-PJD6DRA\Jonny_b <metlin.igor@gmail.com>
2024-05-17 14:30:23 +0300

    Initial commit

---
+++ app/src/main/java/com/simple/ping/MainActivity.kt
@@ -0,0 +1,193 @@
+package com.simple.ping
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Yellow
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.text.isDigitsOnly
+import com.simple.ping.viewmodel.PingViewModel
+
+class MainActivity : ComponentActivity() {
+
+    private val model: PingViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CreateButton(model)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CreateButton(pingViewModel: PingViewModel? = null) {
+    val mosteratFontFamily = FontFamily(Font(R.font.mosterat))
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFF011901),
+                        Color(0xFFaeaeae)
+                    )
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.unnamed),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(200.dp)
+                    .width(200.dp)
+                    .padding(32.dp)
+                    .align(CenterHorizontally)
+            )
+            var text by remember { mutableStateOf(pingViewModel?.interval?.value) }
+            OutlinedTextField(
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedTextColor = Color.White,
+                    unfocusedBorderColor = Color.White,
+                    unfocusedLabelColor = Color.White,
+                    unfocusedLeadingIconColor = Color.White,
+                    focusedBorderColor = Color.White,
+                    cursorColor = Color.White,
+                    focusedTextColor = Color.White
+                ),
+                value = text.toString(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                onValueChange = {
+                    if (it.length <= 6) {
+                        if (it.isDigitsOnly()) text = it
+                        pingViewModel?.interval?.value = it
+                    }},
+                maxLines = 1,
+                singleLine = true,
+                label = { Text(
+                    "Interval in ms default = 500 (max 6 symbols)",
+                    style = TextStyle(color = Color.White))
+                }
+            )
+        }
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize()
+                .padding(20.dp)
+                .align(Center),
+            text = pingViewModel?.text?.value.toString(),
+            color = Color.White,
+            fontFamily = mosteratFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 20.dp)
+                .align(Alignment.BottomCenter),
+            verticalArrangement = Arrangement.Bottom
+        )
+        {
+            createCared("Start Ping", pingViewModel)
+            createCared("Stop Ping", pingViewModel)
+        }
+    }
+}
+@Composable
+private fun createCared(text : String, pingViewModel: PingViewModel? = null){
+    val mosteratFontFamily = FontFamily(Font(R.font.mosterat))
+    val gradient = Brush.horizontalGradient(
+        listOf(
+            Color(0xFF011901),
+            Color(0xFF384938)
+        )
+    )
+    ElevatedCard(elevation = CardDefaults.cardElevation(
+        defaultElevation = 10.dp
+    ),modifier = Modifier
+        .fillMaxWidth()
+        .padding(10.dp)
+        .height(50.dp)
+        .clip(RoundedCornerShape(30.dp))
+        .background(gradient)
+        .clickable (enabled = true){
+            if (text == "Start Ping"){
+                pingViewModel?.startPing()
+                pingViewModel?.text?.value = "Start ping - 8.8.8.8"
+            } else {
+                pingViewModel?.job?.cancel()
+                pingViewModel?.text?.value = "Please press button start ping - 8.8.8.8"
+            }
+        }) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .background(gradient)
+                .clip(RoundedCornerShape(30.dp))
+        ) {
+            Text(
+                color = Color.White,
+                text = text,
+                fontFamily = mosteratFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 20.sp,
+                modifier = Modifier.align(Center)
+            )
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/simple/ping/viewmodel/PingViewModel.kt
@@ -0,0 +1,49 @@
+package com.simple.ping.viewmodel
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.marsounjan.icmp4a.Icmp
+import com.marsounjan.icmp4a.Icmp4a
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+class PingViewModel : ViewModel() {
+
+    var text = mutableStateOf("Please press button start ping - 8.8.8.8")
+    var job: Job? = null
+    var interval = mutableStateOf("500")
+
+    fun startPing() {
+        val host = "google.com"
+        val icmp = Icmp4a()
+        println(interval.value)
+        if (interval.value.isEmpty()){
+            interval.value = "500"
+        }
+        try {
+            job = icmp.pingInterval("8.8.8.8", count = null, intervalMillis = interval.value.toLong())
+                .onEach { status ->
+                    when (val result = status.result) {
+                        is Icmp.PingResult.Success -> Log.d(
+                            "ICMP",
+                            "$host(${status.ip.hostAddress}) ${result.packetSize} bytes - ${result.ms} ms"
+                        )
+
+                        is Icmp.PingResult.Failed -> Log.d(
+                            "ICMP",
+                            "$host(${status.ip.hostAddress}) Failed: ${result.message}"
+                        )
+                    }
+                }
+                .launchIn(viewModelScope)
+        } catch (error: Icmp.Error.UnknownHost) {
+            Log.d("ICMP", "Unknown host $host")
+        }
+    }
+}
